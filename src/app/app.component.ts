b9a74c0ad2d81e0dd796c8c4f675acbb5169a677
fowellsoft <formacion@ANSFORM05.indra.es>
2017-09-19 10:22:45 +0200

    Modificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // Determina al html el tag que deje utilizar para que pegue el contenido
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
